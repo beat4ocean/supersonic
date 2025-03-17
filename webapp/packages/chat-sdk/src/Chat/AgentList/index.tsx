f46a94dd79e0b8dc374f9e1e57c14f557589dde2
beat4ocean <beat4ocean@gmail.com>
2025-03-17 12:38:26 +0800

    去除问答对话 无用添加符号

--- webapp/packages/chat-sdk/src/Chat/AgentList/index.tsx
+++ webapp/packages/chat-sdk/src/Chat/AgentList/index.tsx
@@ -13,15 +13,10 @@ type Props = {
 };
 
 const AgentList: React.FC<Props> = ({ agentList, currentAgent, onSelectAgent }) => {
-  const onAddAgent = () => {
-    message.info('正在开发中，敬请期待');
-  };
-
   return (
     <div className={styles.agentList}>
       <div className={styles.header}>
         <div className={styles.headerTitle}>智能助理</div>
-        <PlusCircleOutlined className={styles.plusIcon} onClick={onAddAgent} />
       </div>
       <div className={styles.agentListContent}>
         {agentList.map((agent, index) => {
